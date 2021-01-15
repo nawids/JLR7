f63a93237628c241d12eab4e458f535d6f89d3b6
Nawid Sahebzadeh <nawids@gmail.com>
2021-01-15 10:57:41 +0100

    Cleaned up the code

--- JLR.js
+++ JLR.js
@@ -1,30 +1,29 @@
-var seconds = 1;
-var on = "1";
-var off= "0";
-var offsetchar = "&nbsp;";
-var offset = 1;
-var ss = "";
-var counter = 0;
+let seconds = 1;
+let on = "1";
+let off = "0";
+let offsetchar = "&nbsp;";
+let offset = 1;
+let ss = "";
+let counter = 0;
 
-var don = "1";
-var ddouble = "0";
-var doff= "0";
+let don = "1";
+let ddouble = "0";
 
 jlr7();
 jlr7date();
 
 function jlr7()
 {
-    var date = new Date();
-    var h = date.getHours(); // 0 - 23
-    var m = date.getMinutes(); // 0 - 59
+    let date = new Date();
+    let h = date.getHours(); // 0 - 23
+    let m = date.getMinutes(); // 0 - 59
     if(h == 0){
         h = 12;
     }
     if(h > 12){
         h = h - 12;
     }
-    var face = createArray();
+    let face = createArray();
     ss = createSymbolArray();
     face = fillArray(face,h,m);
     face[7][seconds] = on;
@@ -34,8 +33,7 @@ function jlr7()
         seconds =1 ;
     }
 
-    var printed = printArray(face);
-    document.getElementById("jlr7display").innerHTML=printed;
+    document.getElementById("jlr7display").innerHTML=printArray(face);
     setTimeout(jlr7, 1000);
 
 }
@@ -43,20 +41,19 @@ function createSymbolArray()
 {
     //The letters used should be JLR7
     //but 7 is now translated to S(even) due to css limitations
-    var s1 = ["J", "L", "R", "S"];
-    var s2 = ["S", "J", "L", "J"];
-    var s3 = ["L", "J", "S", "R"];
-    var s4 = ["R", "S", "J", "S"];
-    var ss = [s1,s2,s3,s4,s1,s2,s3,s4];
-    return ss;
+    let s1 = ["J", "L", "R", "S"];
+    let s2 = ["S", "J", "L", "J"];
+    let s3 = ["L", "J", "S", "R"];
+    let s4 = ["R", "S", "J", "S"];
+    return [s1, s2, s3, s4, s1, s2, s3, s4];
 }
 function createArray()
 {
-    var x = new Array(8);
-    for (var i = 0; i < x.length; i++)
+    let x = new Array(8);
+    for (let i = 0; i < x.length; i++)
     {
         x[i] = new Array(4);
-        for (var j = 0; j < x[i].length; j++)
+        for (let j = 0; j < x[i].length; j++)
         {
             x[i][j] = off;
         }
@@ -65,19 +62,19 @@ function createArray()
 }
 function printArray(array)
 {
-    var x = array;
-    var out = "";
-    for (var i = 0; i < x.length; i++)
+    let x = array;
+    let out = "";
+    for (let i = 0; i < x.length; i++)
     {
         if(offset && i%2)
         {
             out = out+ offsetchar;
         }
-        for (var j = 0; j < x[i].length; j++)
+        for (let j = 0; j < x[i].length; j++)
         {
-            var symbol = ss[i][j];
+            let symbol = ss[i][j];
             out = out +"<span>"+
-                    "<img width=\"30\" height=\"30\" src=\"images/"+symbol+""+x[i][j]+".png\">"+
+                    "<img width=\"30\" height=\"30\" src=\"images/"+symbol+""+x[i][j]+".png\" alt=\"\">"+
                     "&nbsp;</span>";
         }
         out = out + "\n";
@@ -86,12 +83,11 @@ function printArray(array)
 }
 function fillArray(array,h,m)
 {
-    var q = Math.floor(m/15);
+    let q = Math.floor(m / 15);
     m = m - (q*15);
-    var x = array;
-    var out = "";
-    for (var i = 0; i < x.length; i++) {
-      for (var j = 0; j < x[i].length; j++)
+    let x = array;
+    for (let i = 0; i < x.length; i++) {
+      for (let j = 0; j < x[i].length; j++)
       {
         if(h>0)
         {
@@ -117,21 +113,9 @@ function fillArray(array,h,m)
 
     return array;
 }
-function toggleOffset()
-{
-    if(offset)
-    {
-        offset = 0;
-    }
-    else
-    {
-        offset = 1;
-    }
-}
 
 function startAnimation()
 {
-
     counter=0;
     animate();
 }
@@ -141,83 +125,45 @@ function animate()
     if(counter < 7)
     {
         counter++;
-        var face = createArray();
+        let face = createArray();
         face = fillArrayAnimation(face);
-        var printed = printArray(face);
-        document.getElementById("jlr7display").innerHTML=printed;
+        document.getElementById("jlr7display").innerHTML=printArray(face);
         setTimeout(animate, 150);
     }
 }
-function fillArrayAnimation(array,h,m)
+function fillArrayAnimation(array)
 {
-    var x = array;
-    for (var i = 0; i < x.length; i++)
+    let x = array;
+    for (let i = 0; i < x.length; i++)
     {
-        for (var j = 0; j < x[i].length; j++)
+        for (let j = 0; j < x[i].length; j++)
         {
-            var answers = [ on, off];
-            var index = Math.floor(Math.random(1,2) * 2);
+            let answers = [on, off];
+            let index = Math.floor(Math.random() * 2);
             x[i][j] = answers[index];
         }
     }
     return array;
 }
 
-
-
 function jlr7date()
 {
-    var today = new Date();
-    var dd = String(today.getDate()).padStart(2, '0');
-    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    let today = new Date();
+    let dd = String(today.getDate()).padStart(2, '0');
+    let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
 
-    var face = createDateArray();
+    let face = createArray();
     face = fillDateArray(face,mm,dd);
+    document.getElementById("jlr7date_display").innerHTML=printArray(face);
+    setTimeout(jlr7date, 60000);
+}
 
-    var printed = printDateArray(face);
-    document.getElementById("jlr7datedisplay").innerHTML=printed;
-    //setTimeout(jlr7date, 1000);
-}
-function createDateArray()
-{
-    var x = new Array(8);
-    for (var i = 0; i < x.length; i++) {
-      x[i] = new Array(4);
-      for (var j = 0; j < x[i].length; j++)
-      {
-           x[i][j] = doff;
-      }
-    }
-    return x;
-}
-function printDateArray(array)
-{
-    var x = array;
-    var out = "";
-    for (var i = 0; i < x.length; i++) {
-      if(offset && i%2)
-      {
-        out = out+ offsetchar;
-      }
-      for (var j = 0; j < x[i].length; j++)
-      {
-        //out = out +"<span>" +x[i][j]+"</span>";
-        var symbol = ss[i][j];
-        out = out +"<span>"+
-                "<img width=\"30\" height=\"30\" src=\"images/"+symbol+""+x[i][j]+".png\">"+
-                "&nbsp;</span>";
-      }
-      out = out + "\n";
-    }
-    return out;
-}
 function fillDateArray(array,mm,dd)
 {
-    var x = array;
-    var out = "";
-    for (var i = 0; i < x.length; i++)
+    let x = array;
+    for (let i = 0; i < x.length; i++)
     {
-        for (var j = 0; j < x[i].length; j++)
+        for (let j = 0; j < x[i].length; j++)
         {
             if(mm > 0 && mm<=dd)
             {
@@ -233,13 +179,4 @@ function fillDateArray(array,mm,dd)
         }
     }
     return array;
-}
-
-
-function sleep(milliseconds) {
-  const date = Date.now();
-  let currentDate = null;
-  do {
-    currentDate = Date.now();
-  } while (currentDate - date < milliseconds);
 }
